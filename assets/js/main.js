14ca484ac0a83cbebdd270de0354199bcdafc293
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-29 16:45:38 +0200

    add fix array

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,30 +14,28 @@ createApp({
       activeText: 0,
       games:{
 
-        images: [
+        images: ('./assets/img/01.webp',
+        './assets/img/02.webp',
+        './assets/img/03.webp',
+        './assets/img/04.webp',
+        './assets/img/05.webp'),
 
-            './assets/img/01.webp',
-            './assets/img/02.webp',
-            './assets/img/03.webp',
-            './assets/img/04.webp',
-            './assets/img/05.webp'
-        ],
-        titles: [
+        titles: ('Marvel\'s Spiderman Miles Morale',
+        'Ratchet & Clank: Rift Apart',
+        'Fortnite',
+        'Stray',
+        "Marvel's Avengers"),
 
-            'Marvel\'s Spiderman Miles Morale',
-            'Ratchet & Clank: Rift Apart',
-            'Fortnite',
-            'Stray',
-            "Marvel's Avengers",
-        ],
-        texts: [
-
+            
+        
+        texts: (
             'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
             'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
             "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
             'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-            'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-        ]
+            'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.')
+
+        
 
         
     }
@@ -74,7 +72,7 @@ methods: {
     prev(){
 
         console.log(this.games)
-        
+
         this.activeImage--
         if ( activeImage < 0 ) {
 
